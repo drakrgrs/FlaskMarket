651defd6411e384d2ead7150c650dbe91170e330
Drake Rogers <drakrgrs@gmail.com>
2024-12-19 12:31:19 -1000

    converted entire project to previous version (1.1.3) of Flask, and updated the database to include the first two items

--- market.py
+++ market.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
 
 @app.route('/')
 @app.route('/home')
@@ -8,13 +23,22 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
 
-# @app.route('/about/<username>')
-# def about_page(username):
-#     return f'<h1>This is the about page of {username}</h1>'
+@app.route('/add_item', methods=['POST'])
+def add_item():
+    name = request.form.get('name')
+    price = request.form.get('price')
+    barcode = request.form.get('barcode')
+    description = request.form.get('description')
+    new_item = Item(name=name, price=price, barcode=barcode, description=description)
+    db.session.add(new_item)
+    db.session.commit()
+    return redirect(url_for('market_page'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
