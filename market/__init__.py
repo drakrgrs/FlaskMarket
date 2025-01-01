04c9fc805955ed3b2703d5280d0865f11a46fc7a
Drake Rogers <drakrgrs@gmail.com>
2024-12-31 17:50:34 -1000

    avoided the nightmare. Keys are stored properly now with new keys

--- market/__init__.py
+++ market/__init__.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
+import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
 
 from market import routes
