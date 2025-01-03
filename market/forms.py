9c4271c916923adfc2e00cca3c44b10e67ebb296
Drake Rogers <drakrgrs@gmail.com>
2025-01-02 17:35:06 -1000

    wrapping up with registration form

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Length, Email, EqualTo, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Username:')
-    email_address = StringField(label='Email:')
-    password1 = PasswordField(label='Password:')
-    password2 = PasswordField(label='Confirm Password:')
-    submit = SubmitField(label='Submit') 
+    username = StringField(label='Username:', validators=[Length(min=3, max=30), DataRequired()])
+    email_address = StringField(label='Email:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[DataRequired(), EqualTo('password1')])
+    submit = SubmitField(label='Create Account') 
