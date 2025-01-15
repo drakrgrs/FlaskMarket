494fb8c26ed23e11d6e156abf3f4f0f5cbfd3dfb
Drake Rogers <drakrgrs@gmail.com>
2025-01-15 13:52:51 -1000

    finally done

--- market/routes.py
+++ market/routes.py
@@ -16,6 +16,7 @@ def market_page():
     purchase_form = PurchaseItemForm()
     selling_form = SellItemForm()
     if request.method == "POST":
+        #Purachase Item Logic
         purchased_item = request.form.get('purchased_item')
         p_item_object = Item.query.filter_by(name=purchased_item).first()
         if p_item_object:
@@ -24,7 +25,17 @@ def market_page():
                 flash(f"Congratulations! You purchased {p_item_object.name} for {p_item_object.price} successfully!", category='success')
             else:
                 flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}!", category='danger')
-                
+        
+        #Sell Item Logic
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f"Congratulations! You sold {s_item_object.name} back to the market.", category='success')
+            else:
+                flash(f"Something went wrong with selling {s_item_object.name}", category='danger')
+
         return redirect(url_for('market_page'))
 
     if request.method == "GET":
